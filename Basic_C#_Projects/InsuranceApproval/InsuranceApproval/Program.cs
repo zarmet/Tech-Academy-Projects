78ef205445d0bbafbd749a9c88feec8b78dd1056
zarmet <borntoski811@gmail.com>
2021-10-15 18:56:41 -0400

    Insurance Qualification
    Created an Insurance Qualification Application to practice Boolean logic and operators

---
+++ Basic_C#_Projects/InsuranceApproval/InsuranceApproval/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace InsuranceApproval
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Do you qualify for the glorious peonage of Car Insurance TM?\n");
+            Console.WriteLine("What is your age?");
+            string ageinput = Console.ReadLine();
+            int age = Convert.ToInt32(ageinput);
+            Console.WriteLine("Have you ever had a DUI?");
+            string dui = Console.ReadLine();
+            Console.WriteLine("How many speeding tickets do you have?");
+            string tinput = Console.ReadLine();
+            int tickets = Convert.ToInt32(tinput);
+
+            bool agegood = age > 15;
+            bool duigood = dui == "no" || dui == "NO" || dui == "No";
+            bool tickgood = tickets <= 3;
+
+            bool insure = agegood == true && duigood == true && tickgood == true;
+
+            Console.WriteLine("Qualified?\n" + insure);
+        }
+    }
+}
