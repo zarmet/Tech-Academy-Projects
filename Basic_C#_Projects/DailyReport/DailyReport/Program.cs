059e5e768bc7c02fe25f11c9c2adc157e9748105
zarmet <borntoski811@gmail.com>
2021-10-15 17:47:47 -0400

    Added C# Projects
    Added various C# projects including:
    
    -"What is your name?" practice
    
    -Daily Report Assignment
    
    -Misc. C# practice

---
+++ Basic_C#_Projects/DailyReport/DailyReport/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy \n");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string page = Console.ReadLine();
+            int pagenumber = Convert.ToInt32(page);
+            Console.WriteLine("Do you need help with anything? Please answer \"True\" or \"False\".");
+            string help = Console.ReadLine();
+            bool needhelp = Convert.ToBoolean(help);
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string fun = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide?");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string hours = Console.ReadLine();
+            int today = Convert.ToInt32(hours);
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly.Have a great day!");
+        }
+    }
+}
---
+++ Basic_C#_Projects/myConsoleProject/myConsoleProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace myConsoleProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Whhaaaat... is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Whaaaat... is your quest?");
+            string quest = Console.ReadLine();
+            Console.WriteLine("Whaaaat... is the air-speed velocity of an unladen swallow?");
+            string answer = Console.ReadLine();
+            Console.Read();
+
+        }
+    }
+}
