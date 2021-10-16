e5928c45e0af20963c8839acab0c4b5c5f8ecec8
zarmet <borntoski811@gmail.com>
2021-10-16 10:21:44 -0400

    Number Guesser
    Made a number guesser to practice switching and looping

---
+++ Basic_C#_Projects/NumberGuess/NumberGuess/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace NumberGuess
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Can you guess the number I'm thinking of?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool guess = false;
+
+            while (guess == false)
+            {
+                switch (number)
+                {
+                    case 13:
+                        guess = true;
+                        Console.WriteLine("That's it! Great job!");
+                        break;
+                    case 14:
+                        Console.WriteLine("Close, but no cigar. Keep guessing!");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("Close, but no cigar. Keep guessing!");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 69:
+                        Console.WriteLine("Hehe nice, but no. Keep guessing!");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("Keep guessing!");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            
+        }
+    }
+}
