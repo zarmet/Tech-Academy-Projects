7b6289a1e6ffb21934b3584a93094022dd255b9c
zarmet <borntoski811@gmail.com>
2021-10-19 15:40:21 -0400

    Exception Handling
    Practicing try/catch blocks.

---
+++ Basic_C#_Projects/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numbers = new List<int>();
+            numbers.Add(2);
+            numbers.Add(76);
+            numbers.Add(85);
+            numbers.Add(3);
+            numbers.Add(4);
+            numbers.Add(97);
+            numbers.Add(42);
+            numbers.Add(16);
+            try
+            {
+       
+                Console.WriteLine("Enter a divisor:");
+                decimal divisor = Convert.ToDecimal(Console.ReadLine());
+
+                foreach (int number in numbers)
+                {
+                    Console.WriteLine(number / divisor);
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Let's try to preserve the space-time continuum, please don't divide by zero!");
+            }
+            finally
+            {
+                Console.WriteLine("Enter a divisor:");
+                decimal divisor = Convert.ToDecimal(Console.ReadLine());
+
+                foreach (int number in numbers)
+                {
+                    Console.WriteLine(number / divisor);
+                }
+            }
+            
+            
+        }
+    }
+}
