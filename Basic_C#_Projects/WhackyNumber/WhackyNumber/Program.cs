85f33d7723cd80d1897ea225896041b887aa98d8
zarmet <borntoski811@gmail.com>
2021-10-20 14:46:09 -0400

    WhackyNumber
    More practice with classes and methods, this cheeky little program has some randomly goofy little easter eggs.

---
+++ Basic_C#_Projects/WhackyNumber/WhackyNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WhackyNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Number Whackifier\n" +
+                "Enter one or two numbers to get a unique result:");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Want to enter another? You don't have to:");
+            string numTwo = Console.ReadLine();
+            try
+            {
+                int num = Convert.ToInt32(numTwo);
+                Console.WriteLine(Whacky.Number(numOne, num));
+            }
+
+            catch (System.FormatException)
+            {
+                Console.WriteLine(Whacky.Number(numOne));
+            }
+          
+
+        }
+    }
+}
---
+++ Basic_C#_Projects/WhackyNumber/WhackyNumber/Whacky.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhackyNumber
+{
+    class Whacky
+    {
+        public static string Number(int num1, int num2 = 8)
+        {
+            int answer = ((num1 + 2) * 2) * (55 % num2);
+            string strang = Convert.ToString(answer);
+            switch (strang)
+            {
+                case "1":
+                    strang = "Garbanzo!";
+                    break;
+                case "42":
+                    strang = "Tuna!";
+                    break;
+                case "36":
+                    strang = "Bottle!";
+                    break;
+                case "14":
+                    strang = "Paper!";
+                    break;
+                case "22":
+                    strang = "Lamp Shade!";
+                    break;
+                case "64":
+                    strang = "Mutton!";
+                    break;
+                case "8":
+                    strang = "Salad Greens!";
+                    break;
+                case "16":
+                    strang = "Flarpy Darp!";
+                    break;
+                case "616":
+                    strang = "Hail to the Eternal Prince of Darkness! May His reign bring unending Hell fire to God's Kingdom!";
+                    break;
+                default:
+                    strang = strang;
+                    break;
+            }
+            return strang;
+        }
+    }
+}
