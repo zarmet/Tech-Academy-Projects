335ba401e0e9c259f795970ee7f0b2270cf16078
zarmet <borntoski811@gmail.com>
2021-10-22 16:12:29 -0400

    LotsOfClasses
    More practice with classes and methods this is just a simple program to test void and overloaded methods

---
+++ Basic_C#_Projects/LotsOfClasses/LotsOfClasses/Methods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LotsOfClasses
+{
+    class Methods
+    {
+        
+        public void voidMethod(int num=2)
+        {
+            output = num / 2;
+            Console.WriteLine(output);
+        }
+
+        public static int myMethod(int num)
+        {
+            int outpt = num * 2;
+            return outpt;
+        }
+
+        public static int myMethod(int num1, int num2)
+        {
+            int outpt = num1 / num2;
+            return outpt;
+        }
+
+        public int output { get; set; }
+    }
+}
---
+++ Basic_C#_Projects/LotsOfClasses/LotsOfClasses/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LotsOfClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods meth = new Methods();
+            Console.WriteLine("Enter a number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number:");
+            string num2 = Console.ReadLine();
+            Type type = num2.GetType();
+            try
+            {
+                if (type.Equals(typeof(string)))
+                {
+                    int numb = Convert.ToInt32(num2);
+                    Console.WriteLine(Methods.myMethod(num1, numb));
+                }
+                else
+                {
+                    Console.WriteLine(Methods.myMethod(num1));
+                }
+
+            }
+
+            catch (Exception)
+            {
+                meth.voidMethod(num1);
+            }
+
+
+
+            
+
+        }
+    }
+}
