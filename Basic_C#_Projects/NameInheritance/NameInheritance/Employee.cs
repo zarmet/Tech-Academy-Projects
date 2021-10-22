323ec4be7b867812d8374b360383e9b8fc15289c
zarmet <borntoski811@gmail.com>
2021-10-22 16:58:43 -0400

    NameInheritance
    Practiced inheritance and supermethods

---
+++ Basic_C#_Projects/NameInheritance/NameInheritance/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NameInheritance
+{
+    public class Employee:Person
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Basic_C#_Projects/NameInheritance/NameInheritance/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NameInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee student = new Employee() { FirstName = "Sample", LastName = "Student" };
+            student.SayName();
+
+        }
+    }
+}
