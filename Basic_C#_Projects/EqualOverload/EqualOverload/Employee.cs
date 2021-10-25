02ef0fdd0118ed0c11ca5a2bdc1f284033c06ee7
zarmet <borntoski811@gmail.com>
2021-10-25 14:57:01 -0400

    EqualOverload
    Practiced overloading operators by making the == operator compare two employees id numbers

---
+++ Basic_C#_Projects/EqualOverload/EqualOverload/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EqualOverload
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            if (employee1.Id == employee2.Id)
+            {
+                Console.WriteLine("These employees have the same ID Number");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("These employees do not have the same ID Number");
+                return false;
+            }
+
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            if (employee1.Id == employee2.Id)
+            {
+                Console.WriteLine("These employees have the same ID Number");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("These employees do not have the same ID Number");
+                return false;
+            }
+
+        }
+    }
+}
---
+++ Basic_C#_Projects/EqualOverload/EqualOverload/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            int count = 0;
+            while (count < 2)
+            {
+                Console.WriteLine("Please enter the following:");
+                Console.WriteLine("Name:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Identification Number:");
+                int id = Convert.ToInt32(Console.ReadLine());
+                Employee employee = new Employee() { Name = name, Id = id };
+                employees.Add(employee);
+                count += 1;
+            }
+
+            Console.WriteLine(employees[0] == employees[1]);
+            
+        }
+    }
+}
