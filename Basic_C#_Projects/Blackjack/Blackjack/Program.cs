d0d6a4291774004476ae0733e5575cc621de6bf2
zarmet <borntoski811@gmail.com>
2021-10-26 15:33:00 -0400

    LambdaJoe
    Practiced using lambda functions

--- Basic_C#_Projects/Blackjack/Blackjack/Program.cs
+++ Basic_C#_Projects/Blackjack/Blackjack/Program.cs
@@ -13,7 +13,6 @@ namespace Blackjack
             Deck deck = new Deck();
             deck.Shuffle(5);
 
-
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.face + " of " + card.suit);
---
+++ Basic_C#_Projects/LambdaJoe/LambdaJoe/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LambdaJoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee() { FirstName = "Joe", LastName = "Katz", ID = 0004 };
+            Employee emp2 = new Employee() { FirstName = "Stan", LastName = "Hudson", ID = 1546 };
+            Employee emp3 = new Employee() { FirstName = "Lester", LastName = "Jones", ID = 2365 };
+            Employee emp4 = new Employee() { FirstName = "Joe", LastName = "Williams", ID = 0978 };
+            Employee emp5 = new Employee() { FirstName = "Joe", LastName = "Johnson", ID = 1783 };
+            Employee emp6 = new Employee() { FirstName = "Stacy", LastName = "Davis", ID = 7741 };
+            Employee emp7 = new Employee() { FirstName = "Jim", LastName = "Stanhope", ID = 5220 };
+            Employee emp8 = new Employee() { FirstName = "Linda", LastName = "Katz", ID = 9855 };
+            Employee emp9 = new Employee() { FirstName = "Amy", LastName = "Greene", ID = 6127 };
+            Employee emp10 = new Employee() { FirstName = "Laura", LastName = "Potnik", ID = 8819 };
+
+            List<Employee> employees = new List<Employee>() { emp1, emp2, emp3, emp4, emp5, emp6, emp7, emp8, emp9, emp10 };
+            List<Employee> joes = new List<Employee>();
+            List<Employee> mojoes = new List<Employee>();
+            List<Employee> IdGreaterThanFive = new List<Employee>();
+
+
+            foreach (Employee joe in employees)
+            {
+                if (joe.FirstName == "Joe")
+                {
+                    joes.Add(joe);
+                    Console.WriteLine(joe.FirstName + " " + joe.LastName);
+                }
+            }
+
+
+            foreach (Employee person in employees.FindAll(j => j.FirstName == "Joe"))
+            {
+                mojoes.Add(person);
+                Console.WriteLine(person.FirstName + " " + person.LastName);
+            }
+
+            foreach (Employee person in employees.FindAll(j => j.ID > 5))
+            {
+                IdGreaterThanFive.Add(person);
+                Console.WriteLine(person.FirstName + " " + person.LastName + " ID: " + person.ID);
+            }
+        }
+    }
+}
