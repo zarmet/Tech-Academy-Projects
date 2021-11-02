9878b3eebe45dafdd83dea64694a246f2cc909de
zarmet <borntoski811@gmail.com>
2021-11-02 12:12:13 -0400

    DateAndTime
    Practiced using the DateTime struct and learned a valuable lesson about not using keywords for namespaces

---
+++ Basic_C#_Projects/DateAndTime/DateAndTime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DateAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine("It is currently:");
+            Console.WriteLine(now);
+            Console.WriteLine("\nEnter a number:");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            DateTime later = now.AddHours(hours);
+            Console.WriteLine("In " + hours + " hours it will be:");
+            Console.WriteLine(later);
+            
+        }
+    }
+}
